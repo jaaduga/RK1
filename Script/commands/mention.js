93e1ea336ccf1b8d0f85d858338b7c14008bb8ea
jaaduga <bavdinjuneja.7566@gmail.com>
2025-10-25 19:16:45 +0530

    Update mention.js

--- Script/commands/mention.js
+++ Script/commands/mention.js
@@ -13,7 +13,7 @@ module.exports.run = async ({ api, event, args }) => {
  const { mentions, threadID } = event;
 
  if (Object.keys(mentions).length === 0) {
- return api.sendMessage("Boss আপনি কাউকে মেনশন করেননি!\n\nExample: /mention @SA HU 10", threadID);
+ return api.sendMessage("Boss, tumne kisi ko mention nahi kiya!\n\nExample: /mention @FAIZAN 10", threadID);
  }
 
  const mentionID = Object.keys(mentions)[0];
@@ -25,7 +25,7 @@ module.exports.run = async ({ api, event, args }) => {
  for (let i = 0; i < repeatCount; i++) {
  try {
  await api.sendMessage({
- body: `${mentionName}\n\nচিপা থেকে বের হও🐸🔪`,
+ body: `${mentionName}\n\nChhupa se bahar aao🐸🔪`,
  mentions: [{ tag: mentionName, id: mentionID }]
  }, threadID);
  
@@ -33,7 +33,7 @@ module.exports.run = async ({ api, event, args }) => {
  await new Promise(resolve => setTimeout(resolve, 1000)); 
  }
  } catch (error) {
- console.error("মেনশন পাঠাতে সমস্যা:", error);
+ console.error("Mention bhejne me problem:", error);
  break;
  }
  }
